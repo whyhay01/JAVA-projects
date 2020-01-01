4dcdecd93215b0635fb4ee28633b1b38dc02204d
Nafiu Adeseun <adewalenaph@gmail.com>
2020-01-01 15:27:42 +0100

    removed unnecessary comments

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -79,7 +79,5 @@ public class Main {
             }
         }
 
-       // System.out.println("minimumDimension = " + minimumDimension + "  "+"k = " +k+ " effective length =" + effectiveLength
-        //+ " slenderness Ratio = " + slendernessRatio);
     }
 }
